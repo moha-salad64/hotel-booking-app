9bfe4b0a3848f3540ae8cb680c6c154fd10e7f04
moha-salad64 <mohamedsalaad509@gmail.com>
2025-01-15 09:13:56 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-// import 'package:flutter/material.dart';
-// import 'package:hotel_booking_app/components/slider_page.dart';
+import 'package:flutter/material.dart';
+import 'package:hotel_booking_app/components/slider_page.dart';
 
-// void main() {
-//   runApp(
-//     const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: SliderPage(),
-//     )
-//   );
-// }
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SliderPage(),
+    )
+  );
+}
 
