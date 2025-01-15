0f6e49bef80220c19fb5d24fc1e62bd10cf2eb8f
moha-salad64 <mohamedsalaad509@gmail.com>
2025-01-15 10:25:03 +0300

    adding google fonts

--- lib/components/slider_page.dart
+++ lib/components/slider_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SliderPage extends StatelessWidget {
   const SliderPage({super.key});
@@ -6,83 +7,80 @@ class SliderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Hotel Booking Online"),
-      //   centerTitle: true,
-      //   leading: Text("9:10" ,
-      //   style: TextStyle(
-      //     fontSize: 25,
-      //     fontWeight: FontWeight.bold,
-      //   ),
-      //   ),
-      //   actions: [
-      //     Icon(Icons.signal_cellular_0_bar_outlined),
-      //     SizedBox(width: 5),
-      //      Icon(Icons.wifi_outlined),
-      //      SizedBox(width: 5),
-      //     Icon(Icons.battery_charging_full_outlined),
-      //   ],
-      // ),
-
+      backgroundColor: Color(0xffA6CDC6),
     body: ListView(
       children: [
-        Container(
+        Expanded(
+         child:  Container(
           width: double.infinity,
           height: 550,
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('images/slider1.jpeg'),
-              fit: BoxFit.cover,
+              image: AssetImage('images/slider3.jpeg'),
+            fit: BoxFit.cover,
             ),
           ),
           ),
-          SizedBox(height: 45.0,),
-        Container(
+        ),
+        SizedBox(height: 45.0,),
+          Container(
           child: Column(
             children: [
               Text("Booking Hotel as Online" , 
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w800,
-              ),),
-              SizedBox(height: 20),
+              style: GoogleFonts.acme(fontSize: 40, fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20.0),
               Text('Booking the hotel you want without even visiting, all online!',
-              style: TextStyle( fontSize: 17, fontWeight: FontWeight.w800
-              ),),
+              style: GoogleFonts.acme( 
+                fontSize: 18, fontWeight: FontWeight.w800
+              ),
+              ),
             ],),
         ),       
-        SizedBox(height: 30.0),
+        SizedBox(height: 60.0),
         Container(
-          margin: EdgeInsets.only(left: 140.0 , right: 140.0),
+          margin: EdgeInsets.only(left: 60.0 , right: 60.0),
           child:  ElevatedButton(onPressed: (){},
           style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.only(
-              top: 13, bottom: 13,
+            backgroundColor: Colors.grey.shade800,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
             ),
-            backgroundColor: Color(0xff141C24),
+            padding: EdgeInsets.symmetric(
+              vertical: 10, horizontal: 80
+            )
           ),
-         child: Text('Next', style: TextStyle(fontSize: 35 , color: Colors.white),)),
+         child: Text('Next', 
+         style: GoogleFonts.acme(
+          fontSize: 35 , 
+          fontWeight:FontWeight.w500 ,
+           color: Colors.white),)),
         ),
-        SizedBox(height: 30.0),
+        SizedBox(height: 40.0),
         Container(
-          margin: EdgeInsets.only( left: 150 , right: 150),
+          child: Center(
             child: Row(
-            children: [
-             Container(
-              child: OutlinedButton(onPressed: (){}, 
-              child: Text('slider 1')),
-              ),
-
-               
-            ]
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildDot(true),
+                buildDot(false),
+                buildDot(false),
+              ],
+            ),
+          ),
           ),  
-        ),
-        
+        SizedBox(height:20.0),
         Container(
           child: Center(
             child: Column(
               children: [
-                TextButton(onPressed: (){}, 
+                TextButton(onPressed: (){
+                  
+                }, 
                 child: Text('skip' ,
-                style: TextStyle( fontSize: 17 , color: Color(0xff000957)),
+                style: GoogleFonts.acme( fontSize: 17 , color: Color(0xff000957)),
                 )
                 ),
               ],
@@ -90,6 +88,19 @@ class SliderPage extends StatelessWidget {
           ),
         )
         ],),
+    );
+  }
+  
+  Widget buildDot(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      height: 8,
+      width: isActive ? 20 : 8,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.grey.shade800 : Colors.grey.shade600,
+        borderRadius: BorderRadius.circular(5),
+      ),
     );
   }
 }
